e6e5a8b0ade63fb7e72a343f7c23a6582ab442e7
KAYKE AHRENS BISCEGLI <kayke.abiscegli@senacsp.edu.br>
2025-08-20 21:04:27 -0300

    aula03

--- main.cpp
+++ main.cpp
@@ -3,7 +3,81 @@
 #include <GL\glew.h> // GLEW deve estar antes do glfw3.h para evitar conflitos de tipos e definições
 #include <GLFW\glfw3.h>
 
+using namespace std;
+
 const GLint WIDTH = 1024, HEIGHT = 768; // Definindo a largura e altura da janela
+GLuint VAO, VBO, shaderProgram;
+
+// Plota o X,Y
+static const char *vertexShader = "				\n\
+#version 330									\n\
+												\n\
+layout(location=0) in vec2 pos;					\n\
+												\n\
+void main() {									\n\
+	gl_Position = vec4(pos.x, pos.y, 0.0, 1.0); \n\
+}												\n\
+";
+
+// Troca a Cor
+static const char *fragmentShader = "			\n\
+#version 330									\n\
+												\n\
+uniform in vec3 triColor;						\n\
+												\n\
+void main() {									\n\
+	color = vec4(triColor, 1.0);				\n\
+}												\n\
+";
+
+void criaTriangulo() {
+	GLfloat vertices[] = {
+		0.0f, 1.0f,   // Vertice 1
+		-1.0f, -1.0f, // Vertice 2
+		1.0, -1.0f    // Vertice 3
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	  
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		  
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(0); // Location
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+}
+
+void adicionaTriangulo(GLuint program, const char *shaderCode, GLenum type) {
+	GLuint _shader = glCreateShader(type);
+
+	// Converte Char pra GL_CHAR
+	const GLchar *code[1];
+	code[0] = shaderCode;
+
+	glShaderSource(_shader, 1, code, NULL);
+	glCompileShader(_shader);
+
+	// Tratar os Erros
+
+	glAttachShader(program, _shader);
+}
+
+void adicionarPrograma() {
+	shaderProgram = glCreateProgram();
+	if (!shaderProgram) {
+		printf("Erro ao criar o programa");
+		return;
+	}
+
+	adicionaTriangulo(shaderProgram, vertexShader, GL_VERTEX_SHADER);
+	adicionaTriangulo(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+
+	glLinkProgram(shaderProgram);
+}
 
 int main() {
 	// Iniciar o GLFW
